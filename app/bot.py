2b7c1c35ababcbadec185312e65f851c2111caf7
Melve <shvechkovmaxcanada@gmail.com>
2025-07-10 16:10:12 +0600

    recunfigured bot.py

--- app/bot.py
+++ app/bot.py
@@ -10,25 +10,23 @@ from app.services.scheduler import check_websites
 from app.handlers import register_all_routers
 from app.middlewares.db_session_middleware import DbSessionMiddleware
 
-
-WEBHOOK_PATH = '/webhook'
-WEBHOOK_URL = f'https://33e2-46-251-196-144.ngrok-free.app{WEBHOOK_PATH}'
-
-WEB_SERVER_HOST = '0.0.0.0'
-WEB_SERVER_PORT = 8080
+# aiohttp - conf
+# WEBHOOK_PATH = '/webhook'
+# WEBHOOK_URL = f'https://33e2-46-251-196-144.ngrok-free.app{WEBHOOK_PATH}'
+# WEB_SERVER_HOST = '0.0.0.0'
+# WEB_SERVER_PORT = 8080
 
 bot = Bot(token=settings.BOT_TOKEN)
 dp = Dispatcher()
 
 async def on_startup(bot: Bot):
 	"""Выполняется при старте, устанавливает вебхук."""
-	await bot.set_webhook(url=WEBHOOK_URL)
-
-async def main():
+	# await bot.send_webhook(url=WEBHOOK_URL)
 	await bot.delete_webhook(drop_pending_updates=True)
 	await bot.delete_my_commands()
 	await bot.set_my_commands(commands=all_commands)
-	
+
+async def main():
 	dp.callback_query.middleware(DbSessionMiddleware())
 	dp.message.middleware(DbSessionMiddleware())
 	register_all_routers(dp)
@@ -38,21 +36,24 @@ async def main():
 	scheduler.start()
 	
 	dp.startup.register(on_startup)
-	app = web.Application()
 
-	webhook_requests_handler = SimpleRequestHandler(
-		dispatcher=dp,
-		bot=bot
-	)
-	webhook_requests_handler.register(app, path=WEBHOOK_PATH)
-	setup_application(app, dp, bot=bot)
+	await dp.start_polling(bot)
+
+	# app = web.Application()
+
+	# webhook_requests_handler = SimpleRequestHandler(
+	# 	dispatcher=dp,
+	# 	bot=bot
+	# )
+	# webhook_requests_handler.register(app, path=WEBHOOK_PATH)
+	# setup_application(app, dp, bot=bot)
 	
-	runner = web.AppRunner(app)
-	await runner.setup()
-	site = web.TCPSite(runner, host=WEB_SERVER_HOST, port=WEB_SERVER_PORT)
-	await site.start()
+	# runner = web.AppRunner(app)
+	# await runner.setup()
+	# site = web.TCPSite(runner, host=WEB_SERVER_HOST, port=WEB_SERVER_PORT)
+	# await site.start()
 
-	await asyncio.Event().wait()
+	# await asyncio.Event().wait()
 
 
 if __name__ == '__main__':
